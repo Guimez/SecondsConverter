44368dce47701f8a702bd7108fef2d6ce3b75973
Guilherme Módolo <83830962+Guimez@users.noreply.github.com>
2021-07-13 13:49:19 -0300

    Update SecondsConverter.py

--- SecondsConverter.py
+++ SecondsConverter.py
@@ -11,7 +11,7 @@ Input = int(input("Please, enter a value that you want to convert, in seconds: "
 Days = Input // 86400 # How many integer days there is.
 Hours = (Input % 86400) // 3600 # The rest of the above division divided by 3600 (correspondent value of 1 hour in seconds). 
 Minutes = ((Input % 86400) % 3600) // 60 # The rest of the above division divided by 60 (correspondent value of 1 minute in seconds). 
-Seconds = (((Input % 86400) % 3600) % 60) # The rest of the above division
+Seconds = (((Input % 86400) % 3600) % 60) # The rest of the above division.
 
 ### Calculates the values.
 
